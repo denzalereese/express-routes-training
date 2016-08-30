e58c226d839ca1c10a32b815715ed049ff52cd8a
Denzale Reese <dlr249@cornell.edu>
2016-08-29 22:04:33 -0700

    gets the /art-gallery form to post to the server

--- starter-code/public/gallery.js
+++ starter-code/public/gallery.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
+  //$("form").submit(function(event) {
+    /*$.ajax({
+      method: "POST",
+      url: "http://localhost:3000/api/artworks",
+      data: $("#art-form").serialize(),
+      success: handleArtCreateSuccess,
+      error: handleArtCreateError
+    })
+  //});*/
 
 });
+
+function handleArtCreateSuccess(res) {
+  console.log(res);
+  console.log(res.title);
+  console.log(res.artist);
+  console.log(res.description);
+}
+
+function handleArtCreateError() {
+  console.log("DAMN.");
+}
